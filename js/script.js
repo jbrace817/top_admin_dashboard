ee4596a664d0c5672cf32e88f2c34eec006669b3
SM_artist <48393754+jbrace817@users.noreply.github.com>
2023-09-02 09:36:03 -0400

    responsive nav pane

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const nav = document.querySelector(".nav");
+const menu = document.querySelector(".menu");
+const h1 = document.querySelector("h1");
+const logo = document.querySelector(".logo");
+const links = document.querySelectorAll(".links");
+const paras = document.querySelectorAll(".links > p");
+
+menu.addEventListener("click", (e) => {
+  nav.classList.toggle("short");
+  h1.classList.toggle("short");
+
+  if (nav.classList.contains("short")) {
+    logo.style.visibility = "hidden";
+    h1.style.visibility = "hidden";
+    paras.forEach((p) => (p.style.display = "none"));
+  } else {
+    setTimeout(() => {
+      logo.style.visibility = "visible";
+      h1.style.visibility = "visible";
+      paras.forEach((p) => (p.style.display = "block"));
+    }, 200);
+  }
+
+  links.forEach((link) => link.classList.toggle("closed"));
+});
